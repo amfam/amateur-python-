a614d6bd7efa5b8daabaa4d9c5ed08dbd6b80a0b
Amir Faiyaz <42873787+amfam@users.noreply.github.com>
2019-01-16 17:16:31 +0600

    Update lab10.py

--- lab10.py
+++ lab10.py
@@ -4,7 +4,9 @@ Created on Wed Jan 16 16:08:55 2019
 
 @author: Faiyaz
 """
-
-h=int(input('height of triangle: '))
+import math
+a1=int(input('left side of triangle: '))
+a2=int(input('left side of triangle: '))
 f=int(input('floor of triangle: '))
+h=math.sqrt(a1**2-(f/2)**2) #finding height using : a^2=b^2+c^2
 print("area of a triangle :",1/2*h*f)
